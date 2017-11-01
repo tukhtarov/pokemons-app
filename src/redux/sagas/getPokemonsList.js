fe287599395dcef396fe599bf1a608232c4e84c1
User <vlad@allmax.team>
2017-11-01 19:32:36 +0700

    Add api request to get pokemons list

--- src/redux/sagas/getPokemonsList.js
+++ src/redux/sagas/getPokemonsList.js
@@ -1,11 +1,37 @@
-import { put } from 'redux-saga/effects';
+import axios from 'axios';
+import { put, call, all } from 'redux-saga/effects';
 import { setPokemonsList } from '../actions/pokemons';
 
 export default function* getProfilesList() {
   try {
-    //
-    console.log('dddddddd');
-    yield put(setPokemonsList([1,2,3,4]));
+    //const res = yield call(fetch, 'https://pokeapi.co/api/v2/pokemon/');
+    const res = yield call(axios, {
+      method:'get',
+      url:'https://pokeapi.co/api/v2/pokemon/?limit=1000',
+      responseType:'stream'
+    });
+    console.log(res.data.results);
+    const arr = res.data.results.map((pok, ind) => () => call(axios, {
+      method:'get',
+      url:`https://pokeapi.co/api/v2/pokemon/${Number(ind)+1}`,
+      responseType:'stream'
+    }));
+    console.log(arr);
+    const res2 = yield all(arr);
+    // const res2 = yield all([
+    //   call(axios, {
+    //     method:'get',
+    //     url:'https://pokeapi.co/api/v2/pokemon/1',
+    //     responseType:'stream'
+    //   }),
+    //   call(axios, {
+    //     method:'get',
+    //     url:'https://pokeapi.co/api/v2/pokemon/2',
+    //     responseType:'stream'
+    //   })
+    // ]);
+    console.log(res2);
+    yield put(setPokemonsList(res.data.results));
   } catch (error) {
     console.log('ERROR', error);
   }
